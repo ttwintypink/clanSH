a85e5712845728da39c140e0e400491cdb3f8455
ttwintypink <ttwintypink@gmail.com>
2026-02-04 00:32:14 +0300

    Delete SH_discord_bot_split directory

--- SH_discord_bot_split/main.py
+++
@@ -1,7 +0,0 @@
-# main.py
-from config import TOKEN
-from app import client
-import events  # noqa: F401  (важно: регистрирует handlers)
-import slash_commands  # noqa: F401  (важно: регистрирует /add /del /menu)
-
-client.run(TOKEN)
